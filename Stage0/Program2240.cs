f2de439c58df1303b43f47696a73ce058c47a943
TziviMaklev <tm0527171162@gmail.com>
2023-10-26 00:36:53 +0300

    Enter a message <required>

---
+++ Stage0/Program2240.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program2240
+    {
+        static partial void Welcome2240()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}
