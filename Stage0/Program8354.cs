be732bf1a9eee1fb7c289abb51ecdf7e5daee6c6
hadas354 <hadaskatz613@gmail.com>
2023-10-25 21:04:12 +0300

    Initial Code + Welcome functions

---
+++ Stage0/Program8354.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Wellcome8354();
+            Wellcome2240();
+        }
+
+        private static void Wellcome8354()
+        {
+            string name;
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+
+        static partial void Wellcome2240();
+    }
+}
