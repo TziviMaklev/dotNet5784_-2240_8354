fece91be6d7c6195c366d9fd39866a496bac71e0
hadas354 <hadaskatz613@gmail.com>
2023-11-02 21:11:39 +0200

    add records for Task, Engineer, dependency and add their properties

---
+++ DalFacade/DO/Enums.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace DO;
+public enum EngineerExperience 
+{ 
+    Novice, 
+    AdvancedBeginner, 
+    Competent, 
+    Proficient, 
+    Expert 
+}
---
+++ DalFacade/DO/Task.cs
@@ -0,0 +1,20 @@
+﻿
+namespace DO;
+
+public record Task
+(
+    int Id,
+    string Description,
+    string Alias,
+    bool Milestone,
+    DateTime CreationDate,
+    DateTime StartDate,
+    DateTime? ScheduledDate,
+    DateTime? ForecastDate,
+    DateTime? DeadlineDate,
+    DateTime? CompletionDate,
+    string Deliverables = "",
+    string? Remarks = null,
+    int? EngineerId = null,
+    EngineerExperience complexityTask
+);
