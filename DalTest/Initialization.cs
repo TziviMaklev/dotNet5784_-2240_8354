7582486e2ab0938f7b6a8903e2cc8ee8006847d6
hadas354 <hadaskatz613@gmail.com>
2023-11-06 00:17:56 +0200

    add Initialization file for making new list of any entitety

---
+++ DalTest/Initialization.cs
@@ -0,0 +1,18 @@
+﻿using DalApi;
+using DO;
+namespace DalTest;
+
+public static class Initialization
+{
+    private static ITask? s_dalTask;
+    private static IEngineer? s_dalEngineer;
+    private static IDependency? s_dalDependency;
+
+    //use for making random numbers for the objects
+    private static readonly Random s_rand = new();
+
+    private static void createTask()
+    {
+
+    }
+}
