e8782408009cf302a24f04dc7020dc634570d091
TziviMaklev <tm0527171162@gmail.com>
2023-11-05 23:52:11 +0200

    add DependecyImplementation method

--- DalList/DependecyImplementation.cs
+++ DalList/DependecyImplementation.cs
@@ -9,27 +9,50 @@ public class DependecyImplementation : IDependency
 {
     public int Create(Dependency item)
     {
-        throw new NotImplementedException();
+        Dependency? foundEngineer = DataSource.Dependencies.Find(x => x.Id == item.Id);
+        if (foundEngineer == null)
+        {
+            DataSource.Dependencies.Add(foundEngineer);
+        }
+        else
+        {
+            throw new Exception("An Dependency object with such an ID already exists");
+        }
+        return item.Id;
     }
     
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        Dependency? foundEngineer = DataSource.Dependencies.Find(x => x.Id == id);
+        if (foundEngineer != null)
+        {
+            DataSource.Dependencies.Remove(foundEngineer);
+        }
+        else
+        {
+            throw new Exception("An object of type Engineer with such an ID does not exist");
+        }
     }
 
     public Dependency? Read(int id)
     {
-        throw new NotImplementedException();
+        Dependency? foundEngineer = DataSource.Dependencies.Find(x => x.Id == id);
+        if (foundEngineer == null)
+        {
+            return foundEngineer;
+        }
+        return null;
     }
 
     public List<Dependency> ReadAll()
     {
-        throw new NotImplementedException();
+        return new List<Dependency>(DataSource.Dependencies);
     }
 
     public void Update(Dependency item)
     {
-        throw new NotImplementedException();
+        Delete(item.Id);
+        Create(item);
     }
 }
