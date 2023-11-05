cb6365fc2936b30516265792a6e01aa02ac33b87
TziviMaklev <tm0527171162@gmail.com>
2023-11-05 23:47:21 +0200

    finish change

--- DalList/engineerImplementation.cs
+++ DalList/engineerImplementation.cs
@@ -9,26 +9,48 @@ public class EngineerImplementation : IEngineer
 {
     public int Create(Engineer item)
     {
-        throw new NotImplementedException();
+        Engineer? foundEngineer = DataSource.Engineers.Find(x => x.Id == item.Id);
+        if (foundEngineer == null) {
+            DataSource.Engineers.Add(foundEngineer);
+        }
+        else
+        {
+            throw new Exception("An Engineer object with such an ID already exists");
+        }
+        return item.Id;
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        Engineer? foundEngineer = DataSource.Engineers.Find(x => x.Id == id);
+        if (foundEngineer != null)
+        {
+            DataSource.Engineers.Remove(foundEngineer);
+        }
+        else
+        {
+            throw new Exception("An object of type Engineer with such an ID does not exist");
+        }
     }
 
     public Engineer? Read(int id)
     {
-        throw new NotImplementedException();
+        Engineer? foundEngineer = DataSource.Engineers.Find(x => x.Id == id);
+        if (foundEngineer == null)
+        {
+            return foundEngineer;
+        }
+        return null;
     }
 
     public List<Engineer> ReadAll()
     {
-        throw new NotImplementedException();
+        return new List<Engineer> (DataSource.Engineers);
     }
 
     public void Update(Engineer item)
     {
-        throw new NotImplementedException();
+        Delete(item.Id);
+        Create(item);
     }
 }
