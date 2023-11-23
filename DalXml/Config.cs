2677cde7b2dcb00f975bdc98c679cbc5662c8bfa
TziviMaklev <tm0527171162@gmail.com>
2023-11-23 18:39:44 +0200

    change

--- DalXml/Config.cs
+++ DalXml/Config.cs
@@ -5,6 +5,9 @@ namespace Dal;
 internal static class Config
 {
     static string s_data_config_xml = "data-config";
+    static string s_dependencies_xml = "dependencies";
+    static string s_engineers_xml = "engineers";
+    static string s_tasks_xml = "tasks";
     internal static int NextIdDepency { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextIdDepency"); }
     internal static int NextIdTask { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextIdTask"); }
 }
