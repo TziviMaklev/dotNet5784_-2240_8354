3da7b21529e5a6dea284f6984938fb7ddc74c249
TziviMaklev <tm0527171162@gmail.com>
2023-11-23 21:11:44 +0200

    change level 4

--- DalXml/DependencyImplementation.cs
+++ DalXml/DependencyImplementation.cs
@@ -5,37 +5,71 @@ using DalApi;
 using DO;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 internal class DependencyImplementation : IDependency
 {
     public int Create(Dependency item)
     {
-        throw new NotImplementedException();
+        List <Dependency>? Dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>("Dependencies");
+        Dependency? dependency = Dependencies.FirstOrDefault( d => item.Id == d.Id );
+        if (dependency == null)
+        {
+            Dependency newDependency = item;
+            Dependencies?.Add( newDependency );
+            XMLTools.SaveListToXMLSerializer(Dependencies!, "Dependencies");
+        }
+        else
+        {
+            throw new DalAlreadyExistsException("An dependency with this ID number already exists");
+        }
+        return item.Id;
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        List<Dependency>? Dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>("dependencies");
+        Dependency? dependency = Dependencies.FirstOrDefault(d => id == d.Id);
+        if (dependency == null)
+        {
+            throw new DalAlreadyExistsException("An dependency with this ID number not exists");
+
+        }
+        else
+        {
+
+            Dependencies?.Add(dependency);
+            XMLTools.SaveListToXMLSerializer(Dependencies!, "dependencies");
+        }
     }
 
     public Dependency? Read(int id)
     {
-        throw new NotImplementedException();
+        List<Dependency>? dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>("dependencies");
+        return dependencies!.FirstOrDefault(d => d.Id == id);
     }
 
     public Dependency? Read(Func<Dependency, bool> filter)
     {
-        throw new NotImplementedException();
+        List<Dependency>? dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>("dependencies");
+        return dependencies!.FirstOrDefault(filter);
     }
 
     public IEnumerable<Dependency?> ReadAll(Func<Dependency, bool>? filter = null)
     {
-        throw new NotImplementedException();
+        if (filter == null)
+        {
+            return XMLTools.LoadListFromXMLSerializer<Dependency>("dependencies").Select(item => item);
+        }
+        else
+        {
+            return XMLTools.LoadListFromXMLSerializer<Dependency>("dependencies").Where(filter);
+        }
     }
 
     public void Reset()
     {
-        throw new NotImplementedException();
+        
     }
 
     public void Update(Dependency item)
--- DalXml/TaskImplementation.cs
+++ DalXml/TaskImplementation.cs
@@ -9,27 +9,60 @@ internal class TaskImplementation : ITask
 {
     public int Create(Task item)
     {
-        throw new NotImplementedException();
+        List<Task>? tasks = XMLTools.LoadListFromXMLSerializer<Task>("tasks");
+        Task? task = tasks.FirstOrDefault(d => item.Id == d.Id);
+        if (task == null)
+        {
+            Task newTask = item;
+            tasks?.Add(newTask);
+            XMLTools.SaveListToXMLSerializer(tasks!, "tasks");
+        }
+        else
+        {
+            throw new DalAlreadyExistsException("An task with this ID number already exists");
+        }
+        return item.Id;
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        List<Task>? tasks = XMLTools.LoadListFromXMLSerializer<Task>("tasks");
+        Task? task = tasks.FirstOrDefault(d => id == d.Id);
+        if (task == null)
+        {
+            throw new DalAlreadyExistsException("An task with this ID number not exists");
+
+        }
+        else
+        {
+
+            tasks?.Add(task);
+            XMLTools.SaveListToXMLSerializer(tasks!, "dependencies");
+        }
     }
 
     public Task? Read(int id)
     {
-        throw new NotImplementedException();
+        List<Task>? tasks = XMLTools.LoadListFromXMLSerializer<Task>("tasks");
+        return tasks!.FirstOrDefault( t => t.Id == id);
     }
 
     public Task? Read(Func<Task, bool> filter)
     {
-        throw new NotImplementedException();
+        List<Task>? tasks = XMLTools.LoadListFromXMLSerializer<Task>("tasks");
+        return tasks!.FirstOrDefault(filter);
     }
 
     public IEnumerable<Task?> ReadAll(Func<Task, bool>? filter = null)
     {
-        throw new NotImplementedException();
+        if(filter == null)
+        {
+            return XMLTools.LoadListFromXMLSerializer<Task>("tasks").Select(item => item);
+        }
+        else
+        {
+            return XMLTools.LoadListFromXMLSerializer<Task>("tasks").Where(filter);
+        }
     }
 
     public void Reset()
