a22db16a11b74961196210a4e1ca1529c05dc9a7
TziviMaklev <tm0527171162@gmail.com>
2023-12-22 03:04:31 +0200

    currect the pogram

--- BlTest/Program.cs
+++ BlTest/Program.cs
@@ -1,9 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-using BlApi;
+﻿using BlApi;
 using BO;
-using DalApi;
-using IoC;
-using System.Transactions;
+
+
+
 
 internal class Program
 
@@ -52,7 +51,8 @@ internal class Program
             Console.WriteLine(ex.ToString());
         }
     }
-    internal static void printMenu() {
+    internal static void printMenu()
+    {
         Console.WriteLine("Select the method you want to perform\n" +
                           "0. EXIT\n" +
                           "1. List view\n" +
@@ -61,10 +61,12 @@ internal class Program
                           "4. Delete\n" +
                           "5. Update\n");
     }
-    internal static void TaskMethod() {
+    internal static void TaskMethod()
+    {
         printMenu();
         string? ans = Console.ReadLine();
-        int userChose;
+        int userChose, id;
+        bool succesfull;
         int.TryParse(ans, out userChose);
         switch (userChose)
         {
@@ -78,6 +80,14 @@ internal class Program
                 }
                 break;
             case 2:
+                Console.WriteLine("Enter the id of the Task to show: ");
+                ans = Console.ReadLine();
+                succesfull = int.TryParse(ans, out id);
+                if (succesfull)
+                {
+                    BO.Task task1 = s_bl!.Task.GetTask(id)!;
+                    Console.WriteLine(task1);
+                }
                 break;
             case 3:
                 Console.WriteLine("Enter task description: ");
@@ -101,41 +111,40 @@ internal class Program
                     {
                         Console.WriteLine("enter ID of dependency:");
                         ans = Console.ReadLine();
-                        int depId;
-                        bool succed = int.TryParse(ans, out depId);
-                        if (succed)
+                        succesfull = int.TryParse(ans, out id);
+                        if (succesfull)
                         {
                             depps.Add(new TaskInList()
                             {
-                                Id = depId,
+                                Id = id,
                                 Status = 0,
                             });
                         }
                     }
 
                 } while (haveDependency == 1);
-                EngineerExperience experience;
+                BO.EngineerExperience experience;
                 switch (engineerExperience)
                 {
                     case 0:
-                        experience = EngineerExperience.Novice; break;
+                        experience = BO.EngineerExperience.Novice; break;
                     case 1:
-                        experience = EngineerExperience.AdvancedBeginner; break;
+                        experience = BO.EngineerExperience.AdvancedBeginner; break;
                     case 2:
-                        experience = EngineerExperience.Competent; break;
+                        experience = BO.EngineerExperience.Competent; break;
                     case 3:
-                        experience = EngineerExperience.Proficient; break;
+                        experience = BO.EngineerExperience.Proficient; break;
                     case 4:
-                        experience = EngineerExperience.Expert; break;
+                        experience = BO.EngineerExperience.Expert; break;
                     default:
                         throw new ChoiseDoesNotExistException("This choice does not exist");
                 }
                 Console.WriteLine("What is the task duration?");
                 ans = Console.ReadLine();
                 int taskDuration;
-                bool succesful = int.TryParse(ans, out taskDuration);
+                succesfull = int.TryParse(ans, out taskDuration);
                 TimeSpan requiredEffortTime;
-                if (succesful)
+                if (succesfull)
                 {
                     requiredEffortTime = TimeSpan.FromDays(taskDuration);
                 }
@@ -159,52 +168,302 @@ internal class Program
                     DeadlineDate = null,
                     CompleteDate = null,
                     Deliverables = deliver,
-                    ComplexityTask= experience, 
-                    RequiredEffortTime= TimeSpan.FromDays(0),
-
-                    StartDate=null,
+                    Remarks = remarks,
+                    ComplexityTask = experience,
+                    Engineer = null,
                 };
                 break;
-                break;
             case 4:
+                Console.WriteLine("enter id:");
+                ans = Console.ReadLine();
+                succesfull = int.TryParse(ans, out id);
+                if (succesfull)
+                {
+                    s_bl!.Task.RemoveTask(id);
+                }
+                else
+                {
+                    throw new BlDoesNotExistException("An task with this ID number not exists");
+                }
                 break;
             case 5:
-                break;
-            case 6:
+                Console.WriteLine("enter task id:");
+                ans = Console.ReadLine();
+                succesfull = int.TryParse(ans, out id);
+                BO.Task? taskFound = s_bl!.Task.GetTask(id);
+                if (taskFound == null)
+                {
+                    throw new BlDoesNotExistException($"Task with ID={id} does Not exist");
+                }
+
+                Console.WriteLine("if you want change the description enter:");
+                string? description = Console.ReadLine() ?? taskFound.Description;
+
+                Console.WriteLine("if you want change the alias enter:");
+                alias = Console.ReadLine() ?? taskFound.Alias;
+
+                Console.WriteLine("if you want change the milestone enter:");
+                Console.Write("di:");
+                BO.MilstoneInTask milestone;
+                ans = Console.ReadLine() ?? null;
+                if (ans == null)
+                {
+                    milestone = taskFound.Milstone!;
+                }
+                else
+                {
+                    succesfull = int.TryParse(ans, out id);
+                    if (succesfull)
+                    {
+                        Console.Write("alias:");
+                        ans = Console.ReadLine() ?? taskFound.Milstone!.Alias;
+                        milestone = new MilstoneInTask()
+                        {
+                            Id = id,
+                            Alias = ans
+                        };
+                    }
+                    else
+                    {
+                        throw new ThePogramNotSuccedToConvert("the convertof  the id not succed");
+                    }
+
+
+                }
+
+
+                Console.WriteLine("if you want change the deliverables enter:");
+                string? deliverables = Console.ReadLine() ?? taskFound.Deliverables;
+
+                Console.WriteLine("if you want change the complexityTask enter:(0-4)");
+                ans = Console.ReadLine();
+                bool check = int.TryParse(ans, out engineerExperience);
+                EngineerExperience complexityTask;
+                if (!check)
+                    complexityTask = taskFound.ComplexityTask;
+                else
+                    switch (engineerExperience)
+                    {
+                        case 0:
+                            complexityTask = EngineerExperience.Novice; break;
+                        case 1:
+                            complexityTask = EngineerExperience.AdvancedBeginner; break;
+                        case 2:
+                            complexityTask = EngineerExperience.Competent; break;
+                        case 3:
+                            complexityTask = EngineerExperience.Proficient; break;
+                        case 4:
+                            complexityTask = EngineerExperience.Expert; break;
+                        default:
+                            throw new ChoiseDoesNotExistException("This choice does not exist");
+                    }
+
+                Console.WriteLine("if you want change the RequiredEffortTime enter:");
+                ans = Console.ReadLine();
+                check = TimeSpan.TryParse(ans, out requiredEffortTime);
+                if (!check)
+                    requiredEffortTime = taskFound.RequiredEffortTime;
+
+                Console.WriteLine("if you want change the creationDate enter:");
+                DateTime creationDate;
+                ans = Console.ReadLine();
+                check = DateTime.TryParse(ans, out creationDate);
+                if (!check)
+                    creationDate = taskFound.CreatedAtDate;
+
+                Console.WriteLine("if you want change the startDate enter:");
+                DateTime? startDate = taskFound.StartDate;
+                ans = Console.ReadLine();
+                if (ans != "")
+                    startDate = Convert.ToDateTime(ans);
+
+                Console.WriteLine("if you want change the scheduledDate enter:");
+                DateTime? scheduledDate = taskFound.ScheduledDate;
+                ans = Console.ReadLine();
+                if (ans != "")
+                    scheduledDate = Convert.ToDateTime(ans);
+
+                Console.WriteLine("if you want change the deadLineDate enter:");
+                DateTime? deadlineDate = taskFound.DeadlineDate;
+                ans = Console.ReadLine();
+                if (ans != "")
+                    deadlineDate = Convert.ToDateTime(ans);
+
+
+                Console.WriteLine("if you want change the completionDate enter:");
+                DateTime? completionDate = taskFound.CompleteDate;
+                ans = Console.ReadLine();
+                if (ans != "")
+                    completionDate = Convert.ToDateTime(ans);
+
+                Console.WriteLine("if you want change the remarks enter:");
+                remarks = Console.ReadLine();
+                if (remarks == "")
+                    remarks = taskFound.Remarks;
+
+                Console.WriteLine("if you want change the engineerId enter:");
+                BO.EngineerInTask? engineer = taskFound.Engineer;
+                Console.WriteLine("id:");
+                ans = Console.ReadLine();
+                if (ans != "")
+                {
+                    succesfull = int.TryParse(ans, out id);
+                    if (succesfull)
+                    {
+                        Console.WriteLine("name:");
+                        ans = Console.ReadLine() ?? taskFound.Engineer!.Name;
+                        engineer = new EngineerInTask()
+                        {
+                            Id = id,
+                            Name = ans
+                        };
+                    }
+                    else
+                    {
+                        throw new ThePogramNotSuccedToConvert("the convertof  the id not succed");
+                    }
+                }
+                BO.Task boTask = new BO.Task()
+                {
+                    Id = taskFound.Id,
+                    Description = description,
+                    Alias = alias,
+                    Milstone = milestone,
+                    Deliverables = deliverables,
+                    ComplexityTask = complexityTask,
+                    RequiredEffortTime = requiredEffortTime,
+                    CreatedAtDate = creationDate,
+                    StartDate = startDate,
+                    ScheduledDate = scheduledDate,
+                    DeadlineDate = deadlineDate,
+                    CompleteDate = completionDate,
+                    Remarks = remarks,
+                    Engineer = engineer
+                };
+                Console.WriteLine(boTask.Id);
+                s_bl!.Task.UpdateTask(boTask);
                 break;
             default:
                 throw new ChoiseDoesNotExistException("This choice does not exist");
         }
     }
-    internal static void EngineerMethod() {
+    internal static void EngineerMethod()
+    {
         printMenu();
         string? ans = Console.ReadLine();
         int userChose;
         int.TryParse(ans, out userChose);
-        switch (userChose) { 
+        switch (userChose)
+        {
             case 0:
-                 return;
+                return;
             case 1:
+                List<BO.Engineer> engineerL = s_bl.Engineer.RequestEngineersList()!.ToList();
+                Console.WriteLine(string.Join("\n", engineerL));
                 break;
             case 2:
+                Console.WriteLine("Enter the id of the engineer to show: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+                BO.Engineer engineer = s_bl.Engineer?.RequestEngineerDetails(id)!;
+                Console.WriteLine(engineer);
                 break;
             case 3:
+                Console.WriteLine("enter engineer id");
+                id = Convert.ToInt32(Console.ReadLine() ?? throw new NullReferenceException());
+                Console.WriteLine("enter engineer name");
+                string name = Console.ReadLine() ?? throw new NullReferenceException();
+                Console.WriteLine("enter engineer email");
+                string email = Console.ReadLine() ?? throw new NullReferenceException();
+                Console.WriteLine("enter engineer engineerExperience");
+                int engineerExperience = Convert.ToInt32(Console.ReadLine());
+                BO.EngineerExperience level;
+                Console.WriteLine("enter engineer cost");
+                double cost = Convert.ToInt32(Console.ReadLine() ?? throw new NullReferenceException());
+
+                switch (engineerExperience)
+                {
+                    case 0:
+                        level = EngineerExperience.Novice; break;
+                    case 1:
+                        level = EngineerExperience.AdvancedBeginner; break;
+                    case 2:
+                        level = EngineerExperience.Competent; break;
+                    case 3:
+                        level = EngineerExperience.Proficient; break;
+                    case 4:
+                        level = EngineerExperience.Expert; break;
+                    default:
+                        throw new ChoiseDoesNotExistException("This choice does not exist");
+                }
+                Engineer engineer1 = new Engineer()
+                {
+                    Id = id,
+                    Name = name,
+                    Email = email,
+                    Level = level,
+                    Cost = cost
+                };
+                s_bl.Engineer?.AddEngineer(engineer1);
                 break;
             case 4:
+                Console.WriteLine("enter id:");
+                id = Convert.ToInt32(Console.ReadLine());
+                s_bl.Engineer?.RemoveEngineer(id);
                 break;
             case 5:
-                break;
-            case 6:
+                Console.WriteLine("enter task id:");
+                int.TryParse(Console.ReadLine(), out id);
+
+                BO.Engineer engineerFound = s_bl.Engineer?.UpdateEngineerDetails(id)
+                    ?? throw new Exception($"Engineer with ID={id} does Not exist");
+                Console.WriteLine("if you want change the name enter:");
+                name = Console.ReadLine() ?? engineerFound.Name!;
+
+                Console.WriteLine("if you want change the email enter:");
+                email = Console.ReadLine() ?? engineerFound.Email!;
+
+                Console.WriteLine("if you want change the level enter:");
+                bool check = int.TryParse(Console.ReadLine(), out engineerExperience);
+                if (!check)
+                    level = engineerFound.Level;
+                else
+                    switch (engineerExperience)
+                    {
+                        case 0:
+                            level = EngineerExperience.Novice; break;
+                        case 1:
+                            level = EngineerExperience.AdvancedBeginner; break;
+                        case 2:
+                            level = EngineerExperience.Competent; break;
+                        case 3:
+                            level = EngineerExperience.Proficient; break;
+                        case 4:
+                            level = EngineerExperience.Expert; break;
+                        default:
+                            throw new ChoiseDoesNotExistException("This choice does not exist");
+                    }
+                Console.WriteLine("if you want change the cost enter:");
+                double.TryParse(Console.ReadLine(), out cost);
+                engineer1 = new BO.Engineer()
+                {
+                    Id = engineerFound.Id,
+                    Name = name,
+                    Email = email,
+                    Level = level,
+                    Cost = cost
+                };
+                s_bl.Engineer.AddEngineer(engineer1);
                 break;
             default:
                 throw new ChoiseDoesNotExistException("This choice does not exist");
         }
 
     }
-    internal static void MilestoneMethod() {
+    internal static void MilestoneMethod()
+    {
         Console.WriteLine("Select the method you want to perform\n" +
                           "0. EXIT\n" +
-                          "1. creating yhe milestone project schedule \n" +
+                          "1. creating the milestone project schedule \n" +
                           "2. request milestone details\n" +
                           "3. Update\n");
         string? ans = Console.ReadLine();
@@ -215,7 +474,6 @@ internal class Program
             case 0:
                 return;
             case 1:
-
                 break;
             case 2:
                 break;
